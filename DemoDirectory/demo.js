ecd8d420a7ea48a1cf0e62fc73873cc36e4cfcd9
Oliver Anteros <rift95@live.se>
2018-03-13 23:02:30 +0100

    Update demo.js

--- DemoDirectory/demo.js
+++ DemoDirectory/demo.js
@@ -1,43 +1,23 @@
-document.write(`
-        <h2>ToTable(arrayOfObjects)</h2>
-        <pre>ToTable([
+document.querySelector('#table1').innerHTML = 
+	ToTable([
     { firstname: 'Oliver', lastname: 'Twist', age: 14 },
     { firstname: 'Charles', lastname: 'Darwin', age: 78 },
     { firstname: 'Steven', lastname: 'Hawken', age: 50 }
-])</pre>
-        ${ToTable([
-            { firstname: 'Oliver', lastname: 'Twist', age: 14 },
-            { firstname: 'Charles', lastname: 'Darwin', age: 78 },
-            { firstname: 'Steven', lastname: 'Hawken', age: 50 }
-        ])}`);
-        
-        document.write(`
-        <h2>ToTable(arrayOfColumnNames, arrayOfObjects)</h2>
-        <pre>ToTable(['lastname', 'age'], [
+  ]);
+
+document.querySelector('#table2').innerHTML = 
+  ToTable(['lastname', 'age'], [
     { firstname: 'Oliver', lastname: 'Twist', age: 14 },
     { firstname: 'Charles', lastname: 'Darwin', age: 78 },
     { firstname: 'Steven', lastname: 'Hawken', age: 50 }
-])</pre>
-        ${ToTable(['lastname', 'age'], [
-            { firstname: 'Oliver', lastname: 'Twist', age: 14 },
-            { firstname: 'Charles', lastname: 'Darwin', age: 78 },
-            { firstname: 'Steven', lastname: 'Hawken', age: 50 }
-        ])}`);
-        document.write(`
-        <h2>ToTable(objectOfColumnNameMappings, arrayOfObjects)</h2>
-        <pre>ToTable({
+  ]);
+
+document.querySelector('#table3').innerHTML = 
+  ToTable({
     firstname: 'name',
     age: 'age'
-}, [
+  }, [
     { firstname: 'Oliver', lastname: 'Twist', age: 14 },
     { firstname: 'Charles', lastname: 'Darwin', age: 78 },
     { firstname: 'Steven', lastname: 'Hawken', age: 50 }
-])</pre>
-        ${ToTable({
-            firstname: 'name',
-            age: 'age'
-        }, [
-            { firstname: 'Oliver', lastname: 'Twist', age: 14 },
-            { firstname: 'Charles', lastname: 'Darwin', age: 78 },
-            { firstname: 'Steven', lastname: 'Hawken', age: 50 }
-        ])}`);
+  ]);
